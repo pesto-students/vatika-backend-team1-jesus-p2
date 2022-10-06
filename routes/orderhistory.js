6ffa02f98b10148b6d6f420490b5c606ecee75bb
Kavish15 <kavishgarg15@gmail.com>
2022-10-06 17:18:41 +0530

    Adding File

--- routes/orderhistory.js
+++
@@ -1,9 +0,0 @@
-const router=require('express').Router();
-const verify=require('../middleware/auth')
-const {userOrderHistory}=require('../controllers/orderhistory')
-
-
-router.get('/orderhistory',verify,userOrderHistory)
-
-
-module.exports=router;
--- routes/user.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const { userEmailVerify, userSignUp, userLogin } = require("../controllers/user");
-
-
-router.get("/verify", userEmailVerify); //New User EmailVerification
-router.post("/signup", userSignUp); //New User Signup
-router.post("/login", userLogin); //Existing User Login
-
-module.exports = router;
