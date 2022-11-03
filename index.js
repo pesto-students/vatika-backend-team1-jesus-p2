cca14eeb4c4ad1bb70526e685c5468bc5679f1ef
Kavish15 <kavishgarg15@gmail.com>
2022-11-03 21:40:27 +0530

    Added Confirmation Page and Updated ReadMe File

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
 const bodyParser = require("body-parser");
+const path =require("path");
 const cors = require("cors");
 app.use(
   cors({
@@ -29,6 +30,8 @@ mongoose.connect(process.env.DB_URL, () => {
 //Middleware
 app.use(express.json()); //this is to accept data in JSON Format
 app.use(bodyParser.urlencoded({ extended: true })); //this is to decode the data sent from FrontEnd
+app.use(express.static(path.join(__dirname,"public")));
+
 
 app.use("/", orderhistoryRouter); //orderhistoryRoute Middleware
 app.use("/", productRouter); //addProductRoute Middleware
@@ -38,6 +41,16 @@ app.use("/", superCoinRouter); //superCoinRoute Middleware
 app.use("/", paymentRouter); //paymentRoute Middleware
 app.use("/api/signup", signupRouter); //signUpRoute Middleware
 app.use("/api/login", loginRouter); // loginRoute Middleware
+
+//Default Router
+app.get("/", (req, res) => {
+  res.send("Welcome to Vatika");
+});
+
+//Confirmation
+app.post("/confirmation", (req, res) => {
+  res.status(200).sendFile(path.join(__dirname,"public","confirm.html"));
+});
 
 //Listening to Port
 const server=app.listen(process.env.PORT || 5000, () => {
